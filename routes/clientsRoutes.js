d32054804575a3282a2c7572b623c13a30e37850
Felipe <felitooliva2@gmail.com>
2024-10-15 19:09:29 -0300

    agregado de /

--- routes/clientsRoutes.js
+++ routes/clientsRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const clientControllers = require("../controllers/clientsControllers");
 
+router.get("/", (req, res) => {
+  res.send({ message: "Welcome to fullstack-crud-auth0-back!" });
+});
 router.get("/clientes", clientControllers.getAllClients);
 router.post("/addClient", clientControllers.addClient);
 router.put("/dropClient/:ID", clientControllers.dropClient);
